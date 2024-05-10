8d5081e056266e66252333fa94b0222ba7e0c951
Christopher Wiggins <cwiggins0217@protonmail.com>
2024-05-09 20:28:40 -0500

    tweak tmux conf to follow terminal scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ if wezterm.config_builder then config = wezterm.config_builder() end
 -- Settings
 config.color_scheme = "Desert"
 config.font = wezterm.font_with_fallback({
-  {family = "Inconsolata Nerd Font Mono", scale = 1.75 },
+  {family = "Input Mono", scale = 1.75 },
   {family = "Hack", scale = 1.75 },
 })
 config.window_background_opacity = 0.75
@@ -16,6 +16,7 @@ config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
 config.enable_tab_bar = false
+config.enable_wayland = true
 
 -- keys
 config.leader = {key = "b", mods = "CTRL", timetout_milliseconds = 1000 }
