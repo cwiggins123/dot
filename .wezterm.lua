630ebaa57628d63868c85076113071447199efcb
christopher <christopher@christopher-A520I-AC.attlocal.net>
2024-05-11 13:12:34 -0500

    fix wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,13 +5,13 @@ local config = {}
 if wezterm.config_builder then config = wezterm.config_builder() end 
 
 -- Settings
-config.color_scheme = "Tomorrow Night Eighties"
+config.color_scheme = "OneHalfDark"
 config.font = wezterm.font_with_fallback({
   {family = "Input Mono", scale = 1.75 },
   {family = "Hack", scale = 1.75 },
 })
 config.window_background_opacity = 0.85
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
