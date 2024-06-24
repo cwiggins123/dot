715ca3b74666e164d11e8e7e59bc50bd7c66584b
Christopher Wiggins <cwiggins0217@protonmail.com>
2024-06-24 11:07:10 -0500

    cleanup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then config = wezterm.config_builder() end
-config.color_scheme = "Oxocarbon Dark (Gogh)"
+config.color_scheme = "3024 (base16)"
 config.font = wezterm.font_with_fallback({
   { family = "Liga SFMono Nerd Font", scale = 1.5 },
   { family = "Monospace", scale = 1.5 },
